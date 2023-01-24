8d33d15fd6b890bfa97e1718f0a8fc2ede6dc9fd
jomingyun <jmg97@kakao.com>
2023-01-24 21:59:52 +0900

    fix 오타수정

--- src/header/headerNavigation.js
+++ src/header/headerNavigation.js
@@ -11,7 +11,7 @@ export function HeaderNavigation() {
                 </li>
                 <li className="menu-list">
                     <a className="menu-font" href="https://www.google.com">
-                        INFOR
+                        INFO
                     </a>
                 </li>
                 <li className="menu-list">
