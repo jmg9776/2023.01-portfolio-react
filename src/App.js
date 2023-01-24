81c15066791932db03aff5eec15931305c217216
jomingyun <jmg97@kakao.com>
2023-01-24 22:11:32 +0900

    fix 브라우저 확인

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import BrowserDetection from "https://cdn.skypack.dev/react-browser-detection@1.2.1"
 import Content from './contents/contents.js'
 
-const browserHandler = {
-    chrome: () => "",
-    default: (browser) => alert("이 웹페이지는 데스크탑 전용이며\n Chrome 브라우저에서만 테스트 되었습니다." +
-        "\n 다른 브라우저 이용시 오류가 발생할 수 있습니다.")
-}
 function App() {
+    let sUsrAg = navigator.userAgent;
+
+    if (sUsrAg.indexOf("Chrome") > -1) {
+    } else {
+        alert("이 웹페이지는 데스크탑 전용입니다.\n" +
+            "크롬 브라우저 기준으로 작성되었습니다.\n" +
+            "다른 브라우저 이용시 정상적인 확인이 불가능 할 수 있습니다.")
+    }
     return (<div>
-            <BrowserDetection>
-                {browserHandler}
-            </BrowserDetection>
             <div className="wrap">
                 <Content></Content>
             </div>
